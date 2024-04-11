ac6c97502bdac5b676ae3111f486d629dc1cd9bd
Poper84 <jakegrimsman@yahoo.com>
2024-04-11 10:53:23 -0600

    Security for Members

--- IntexII_11/Controllers/HomeController.cs
+++ IntexII_11/Controllers/HomeController.cs
@@ -5,6 +5,7 @@ using Microsoft.AspNetCore.Authorization;
 using Microsoft.ML.OnnxRuntime;
 using Microsoft.ML.OnnxRuntime.Tensors;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
 
 namespace IntexII_11.Controllers;
 
@@ -18,7 +19,9 @@ public class HomeController : Controller
         _repo = temp;
         try
         {
-            _session = new InferenceSession("/Users/alexfankhauser/Documents/GitHub/IntextII_11/IntexII_11/random_forest_classifier.onnx");
+            string currentDirectory = Directory.GetCurrentDirectory();
+            string onnxFilePath = Path.Combine(currentDirectory, "random_forest_classifier.onnx");
+            _session = new InferenceSession(onnxFilePath);
         }
         catch (Exception)
         {
@@ -26,6 +29,7 @@ public class HomeController : Controller
             throw;
         }
     }
+
 
     [HttpGet]
     public IActionResult Index(string[] category, string primaryColor, string secondaryColor)
@@ -58,7 +62,7 @@ public class HomeController : Controller
         return View();
     }
 
-    [HttpGet]
+    [Authorize(Roles = "Member")]
     public IActionResult Checkout()
     {
         return View();
